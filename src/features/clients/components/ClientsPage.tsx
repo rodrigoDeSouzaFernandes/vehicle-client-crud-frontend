dc12d63140570009e564313e4a3c5e6c644d2d90
Rodrigo <rdesouza2008@gmaill.com>
2026-02-11 18:59:22 -0300

    chore: remove unused import

--- src/features/clients/components/ClientsPage.tsx
+++ src/features/clients/components/ClientsPage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, useEffect } from 'react';
+import { useCallback, useState } from 'react';
 import { useClients } from '../hooks';
 import { useToast } from '@/core/providers';
 import ClientsTable from './ClientsTable';
